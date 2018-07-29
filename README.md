c538b725bba0f2424fef140cd7671b6a595adefc
overlookmotel <theoverlookmotel@gmail.com>
2018-07-29 20:00:10 +0100

    Initial release

--- README.md
+++ README.md
@@ -11,7 +11,61 @@
 [![Greenkeeper badge](https://badges.greenkeeper.io/overlookmotel/co-class.svg)](https://greenkeeper.io/)
 [![Coverage Status](https://img.shields.io/coveralls/overlookmotel/co-class/master.svg)](https://coveralls.io/r/overlookmotel/co-class)
 
+## What's it for?
+
+In versions of Node without `async/await` support, co-routines can be used to implement the same functionality using generator functions.
+
+But defining class methods as co-routines is a pain. This module solves that problem.
+
 ## Usage
+
+If you're on a version of Node that doesn't support `async/await`, instead of this code:
+
+```js
+class MyClass {
+  async doSomethingAsync(a, b, c) {
+    const res = await Promise.resolve(a * b * c);
+    return res;
+  }
+}
+```
+
+...write this instead:
+
+```js
+const coClass = require('co-class');
+
+const MyClass = coClass( class {
+  *doSomethingAsync(a, b, c) {
+    const res = yield Promise.resolve(a * b * c);
+    return res;
+  }
+} );
+```
+
+### How it works
+
+All class methods (both instance methods and static methods) which are generator functions are converted into co-routines.
+
+Co-routines are equivalent to `async/await` syntax. Wrapping is performed using [co-simple](https://www.npmjs.com/package/co-simple).
+
+`super` also works as you'd expect for calling methods on a super-class.
+
+### Wrapping just instance or static methods
+
+To wrap only static methods:
+
+```js
+coClass.static( klass );
+```
+
+To wrap on instance methods:
+
+```js
+coClass.instance( klass );
+```
+
+Calling `coClass( klass )` is equivalent to calling both the above methods.
 
 ## Tests
 
--- test/all.test.js
+++ test/all.test.js
@@ -8,18 +8,264 @@
 // Modules
 const chai = require('chai'),
 	{expect} = chai,
+	chaiAsPromised = require('chai-as-promised'),
 	coClass = require('../lib/');
 
 // Init
 chai.config.includeStack = true;
+chai.use(chaiAsPromised);
 
 // Tests
 
 /* jshint expr: true */
-/* global describe, it */
+/* global describe, it, beforeEach */
 
-describe('Tests', function() {
-	it.skip('all', function() {
-		expect(coClass).to.be.ok;
+function makeClass() {
+	class RootKlass {
+		subclassedInstanceMethod() {
+			return 56;
+		}
+
+		static subclassedStaticMethod() {
+			return 78;
+		}
+	}
+
+	class Klass extends RootKlass {
+		*asyncInstanceMethod(a, b) {
+			return yield Promise.resolve(a + b);
+		}
+
+		static *asyncStaticMethod(a, b) {
+			return yield Promise.resolve(a + b);
+		}
+
+		syncInstanceMethod(a, b) {
+			return a + b;
+		}
+
+		static syncStaticMethod(a, b) {
+			return a + b;
+		}
+
+		*subclassedInstanceMethod() {
+			return super.subclassedInstanceMethod();
+		} // jshint ignore:line
+
+		static *subclassedStaticMethod() {
+			return super.subclassedStaticMethod();
+		} // jshint ignore:line
+	}
+
+	Klass.prototype.instanceProp = 12;
+	Klass.staticProp = 34;
+
+	return Klass;
+}
+
+describe('coClass()', function() {
+	beforeEach(function() {
+		const Klass = makeClass();
+		this.originalKlass = Klass;
+		this.Klass = coClass(Klass);
+		this.instance = new Klass();
+	});
+
+	it('returns input', function() {
+		expect(this.Klass).to.equal(this.originalKlass);
+	});
+
+	describe('wraps instance methods', function() {
+		it('to function', function() {
+			const {instance} = this;
+			expect(instance.asyncInstanceMethod).to.be.a('function');
+		});
+
+		it('function returns promise', function() {
+			const {instance} = this;
+			const p = instance.asyncInstanceMethod(3, 4);
+			expect(p).to.be.instanceof(Promise);
+			return expect(p).to.eventually.equal(7);
+		});
+
+		it('maintains `super` relationship', function() {
+			const {instance} = this;
+			const p = instance.subclassedInstanceMethod();
+			return expect(p).to.eventually.equal(56);
+		});
+	});
+
+	describe('wraps static methods', function() {
+		it('to function', function() {
+			const {Klass} = this;
+			expect(Klass.asyncStaticMethod).to.be.a('function');
+		});
+
+		it('function returns promise', function() {
+			const {Klass} = this;
+			const p = Klass.asyncStaticMethod(3, 4);
+			expect(p).to.be.instanceof(Promise);
+			return expect(p).to.eventually.equal(7);
+		});
+
+		it('maintains `super` relationship', function() {
+			const {Klass} = this;
+			const p = Klass.subclassedStaticMethod();
+			return expect(p).to.eventually.equal(78);
+		});
+	});
+
+	describe('leaves unchanged', function() {
+		it('instance methods', function() {
+			const {instance} = this;
+			expect(instance.syncInstanceMethod).to.be.a('function');
+			expect(instance.syncInstanceMethod(3, 4)).to.equal(7);
+		});
+
+		it('static methods', function() {
+			const {Klass} = this;
+			expect(Klass.syncStaticMethod).to.be.a('function');
+			expect(Klass.syncStaticMethod(3, 4)).to.equal(7);
+		});
+
+		it('instance properties', function() {
+			const {instance} = this;
+			expect(instance.instanceProp).to.equal(12);
+		});
+
+		it('static properties', function() {
+			const {Klass} = this;
+			expect(Klass.staticProp).to.equal(34);
+		});
+	});
+});
+
+describe('coClass.static()', function() {
+	beforeEach(function() {
+		const Klass = makeClass();
+		this.originalKlass = Klass;
+		this.Klass = coClass.static(Klass);
+		this.instance = new Klass();
+	});
+
+	it('returns input', function() {
+		expect(this.Klass).to.equal(this.originalKlass);
+	});
+
+	describe('wraps static methods', function() {
+		it('to function', function() {
+			const {Klass} = this;
+			expect(Klass.asyncStaticMethod).to.be.a('function');
+		});
+
+		it('function returns promise', function() {
+			const {Klass} = this;
+			const p = Klass.asyncStaticMethod(3, 4);
+			expect(p).to.be.instanceof(Promise);
+			return expect(p).to.eventually.equal(7);
+		});
+
+		it('maintains `super` relationship', function() {
+			const {Klass} = this;
+			const p = Klass.subclassedStaticMethod();
+			return expect(p).to.eventually.equal(78);
+		});
+	});
+
+	describe('leaves unchanged', function() {
+		it('instance methods', function() {
+			const {instance} = this;
+			expect(instance.syncInstanceMethod).to.be.a('function');
+			expect(instance.syncInstanceMethod(3, 4)).to.equal(7);
+		});
+
+		it('instance generator methods', function() {
+			const {instance} = this;
+			expect(instance.asyncInstanceMethod).to.be.a('function');
+			const res = instance.asyncInstanceMethod();
+			expect(res.next).to.be.a('function');
+			expect(res.throw).to.be.a('function');
+		});
+
+		it('static methods', function() {
+			const {Klass} = this;
+			expect(Klass.syncStaticMethod).to.be.a('function');
+			expect(Klass.syncStaticMethod(3, 4)).to.equal(7);
+		});
+
+		it('instance properties', function() {
+			const {instance} = this;
+			expect(instance.instanceProp).to.equal(12);
+		});
+
+		it('static properties', function() {
+			const {Klass} = this;
+			expect(Klass.staticProp).to.equal(34);
+		});
+	});
+});
+
+describe('coClass.instance()', function() {
+	beforeEach(function() {
+		const Klass = makeClass();
+		this.originalKlass = Klass;
+		this.Klass = coClass.instance(Klass);
+		this.instance = new Klass();
+	});
+
+	it('returns input', function() {
+		expect(this.Klass).to.equal(this.originalKlass);
+	});
+
+	describe('wraps instance methods', function() {
+		it('to function', function() {
+			const {instance} = this;
+			expect(instance.asyncInstanceMethod).to.be.a('function');
+		});
+
+		it('function returns promise', function() {
+			const {instance} = this;
+			const p = instance.asyncInstanceMethod(3, 4);
+			expect(p).to.be.instanceof(Promise);
+			return expect(p).to.eventually.equal(7);
+		});
+
+		it('maintains `super` relationship', function() {
+			const {instance} = this;
+			const p = instance.subclassedInstanceMethod();
+			return expect(p).to.eventually.equal(56);
+		});
+	});
+
+	describe('leaves unchanged', function() {
+		it('instance methods', function() {
+			const {instance} = this;
+			expect(instance.syncInstanceMethod).to.be.a('function');
+			expect(instance.syncInstanceMethod(3, 4)).to.equal(7);
+		});
+
+		it('static methods', function() {
+			const {Klass} = this;
+			expect(Klass.syncStaticMethod).to.be.a('function');
+			expect(Klass.syncStaticMethod(3, 4)).to.equal(7);
+		});
+
+		it('static generator methods', function() {
+			const {Klass} = this;
+			expect(Klass.asyncStaticMethod).to.be.a('function');
+			const res = Klass.asyncStaticMethod();
+			expect(res.next).to.be.a('function');
+			expect(res.throw).to.be.a('function');
+		});
+
+		it('instance properties', function() {
+			const {instance} = this;
+			expect(instance.instanceProp).to.equal(12);
+		});
+
+		it('static properties', function() {
+			const {Klass} = this;
+			expect(Klass.staticProp).to.equal(34);
+		});
 	});
 });
